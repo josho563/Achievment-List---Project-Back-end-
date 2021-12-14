6390242da416415bebd8af33f1d65f22dced42f5
josh <joshowillmott@outlook.com>
2021-12-14 09:45:34 +0000

    ALP-28

--- Spring-AchievementProject/src/main/java/com/bae/achievement/service/AchievementService.java
+++ Spring-AchievementProject/src/main/java/com/bae/achievement/service/AchievementService.java
@@ -1,5 +1,21 @@
 package com.bae.achievement.service;
 
-public class AchievementService {
+import java.util.List;
 
+import com.bae.achievement.domain.Achievement;
+
+public interface AchievementService {
+
+	//Create
+	Achievement createAchievement(Achievement achievement);
+
+	//Read All
+	List<Achievement> getAllAchievements();
+
+	// Update
+	Achievement replaceAchievement(Integer id, Achievement newAchievement);
+
+	// Delete
+	void removeAchievement(Integer id);
 }
+

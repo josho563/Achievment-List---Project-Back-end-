cb9f845741e35cd4bed6711ce3a92a44f612901d
josh <joshowillmott@outlook.com>
2021-12-14 08:24:14 +0000

    ALP-28

---
+++ Spring-AchievementProject/src/main/java/com/bae/achievement/SpringAchievementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bae.achievement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAchievementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAchievementProjectApplication.class, args);
+	}
+
+}
---
+++ Spring-AchievementProject/src/test/java/com/bae/achievement/SpringAchievementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.achievement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAchievementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
